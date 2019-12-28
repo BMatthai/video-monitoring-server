de93f4df933799ef4ceebe16ad45926ee3fcb34e
Bastien MATTHÄI <bastien_matthai1@hotmail.fr>
2019-12-28 01:04:25 +0100

    fix3

--- politiquement_correct.py
+++ politiquement_correct.py
@@ -48,20 +48,13 @@ def shape_detection():
 
 	encode_param = [int(cv2.IMWRITE_JPEG_QUALITY), 90]
 
-	cam = cv2.VideoCapture(0)
-	cam.set(3, 320);
-	cam.set(4, 240);
-	img_counter = 0
-
 	while True:
-	    ret, frame = cam.read()
-	    result, frame = cv2.imencode('.jpg', frame, encode_param)
-	#    data = zlib.compress(pickle.dumps(frame, 0))
-	    data = pickle.dumps(frame, 0)
-	    size = len(data)
+		frame = camera.capture()
+		result, frame = cv2.imencode('.jpg', frame, encode_param)
+		data = pickle.dumps(frame, 0)
+		size = len(data)
 		print("{}: {}".format(img_counter, size))
-	    client_socket.sendall(struct.pack(">L", size) + data)
-	    img_counter += 1
-	cam.release()
+		client_socket.sendall(struct.pack(">L", size) + data)
+	camera.release()
 
 shape_detection()

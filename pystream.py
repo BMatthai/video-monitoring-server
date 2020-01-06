6255ef12c9a7affdddb8c0ab2911b262b1b59607
Bastien <bastien_matthai1@hotmail.fr>
2020-01-06 18:39:57 +0100

    fix indentation

--- pystream.py
+++ pystream.py
@@ -14,17 +14,17 @@ HOST=''
 PORT=8554
 
 def timestamp_minute():
-        """
-        This method returns the cur timestamp as minute, in a entire value.
-        """
-        return int(datetime.timestamp(datetime.now()))
+		"""
+		This method returns the cur timestamp as minute, in a entire value.
+		"""
+		return int(datetime.timestamp(datetime.now()))
 
 # Tant que 1:
-    # Si mouvement:
-        # Si non recording:
-            # Commencer recording
-        # Si recording
-            #  D  caller fin recording de X minutes.
+	# Si mouvement:
+		# Si non recording:
+			# Commencer recording
+		# Si recording
+			#  D  caller fin recording de X minutes.
 
 def shape_detection():
 	# initialize the camera and grab a reference to the raw camera capture
@@ -57,16 +57,16 @@ def shape_detection():
 		for (x,y,w,h) in eye:
 			frame = cv2.rectangle(frame, (x, y), (x + w, y + h), color, thickness) 
 			if (isRecording == False):
-                camera.start_recording('./recording/VIDEO_' + str(timestamp_minute()) + '.h264')
-        		isRecording = True
-                endRecording = timestamp_minute() + COOLDOWN_DURATION
+				camera.start_recording('./recording/VIDEO_' + str(timestamp_minute()) + '.h264')
+				isRecording = True
+				endRecording = timestamp_minute() + COOLDOWN_DURATION
 			else:
-		    	endRecording = timestamp_minute() + COOLDOWN_DURATION
+				endRecording = timestamp_minute() + COOLDOWN_DURATION
 
 		result, frame = cv2.imencode('.jpg', frame, encode_param)
 
 		data = pickle.dumps(frame, 0)
-   		size = len(data)
+		size = len(data)
 		# print("{}: {}".format(img_counter, size))
 		s.sendall(struct.pack(">L", size) + data)
 		# img_counter += 1

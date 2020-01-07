e4f82cfdb7ef5e38065346b55bf2848f5bdd6594
Bastien <bastien_matthai1@hotmail.fr>
2020-01-08 00:31:22 +0100

    with Miguel works

--- pystream.py
+++ pystream.py
@@ -1,21 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 from flask import Flask, render_template, Response
-from video import Video
+from picamera import Camera
 
 app = Flask(__name__)
-vid=Video()
 
-def gen():
-	for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
-		yield (b'--frame\r\n'
-			   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+def gen(camera):
+    while True:
+        frame = camera.get_frame()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
 
 @app.route('/')
-def video():
-	return render_template('index.html')
-
-@app.route('/video_feed')
 def video_feed():
-	return Response(gen(), mimetype='multipart/x-mixed-replace; boundary=frame')
-
-if __name__ == '__main__':
-	app.run(host='0.0.0.0', debug=True)
+    return Response(gen(Camera()),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')

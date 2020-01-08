e47360f1372fe1989541a9fbf7e97b60e3c0de06
Bastien <bastien_matthai1@hotmail.fr>
2020-01-08 14:12:35 +0100

    test cv reindent

--- picamera.py
+++ picamera.py
@@ -9,60 +9,60 @@ import picamera
 import cv2
 
 class Camera(object):
-    thread = None  # background thread that reads frames from camera
-    frame = None  # current frame is stored here by background thread
-    last_access = 0  # time of last client access to the camera
+	thread = None  # background thread that reads frames from camera
+	frame = None  # current frame is stored here by background thread
+	last_access = 0  # time of last client access to the camera
 
-    def initialize(self):
-        if Camera.thread is None:
-            # start background frame thread
-            Camera.thread = threading.Thread(target=self._thread)
-            Camera.thread.start()
+	def initialize(self):
+		if Camera.thread is None:
+			# start background frame thread
+			Camera.thread = threading.Thread(target=self._thread)
+			Camera.thread.start()
 
-            # wait until frames start to be available
-            while self.frame is None:
-                time.sleep(0)
+			# wait until frames start to be available
+			while self.frame is None:
+				time.sleep(0)
 
-    def get_frame(self):
-        Camera.last_access = time.time()
-        self.initialize()
-        return self.frame
+	def get_frame(self):
+		Camera.last_access = time.time()
+		self.initialize()
+		return self.frame
 
-    @classmethod
-    def _thread(cls):
-        with picamera.PiCamera() as camera:
-            # camera setup
-            camera.resolution = (320, 240)
-            camera.hflip = True
-            camera.vflip = True
+	@classmethod
+	def _thread(cls):
+		with picamera.PiCamera() as camera:
+			# camera setup
+			camera.resolution = (320, 240)
+			camera.hflip = True
+			camera.vflip = True
 
-            # let camera warm up
-            camera.start_preview()
-            time.sleep(2)
+			# let camera warm up
+			camera.start_preview()
+			time.sleep(2)
 
-            stream = io.BytesIO()
-            eye_cascade = cv2.CascadeClassifier('./haarcascades/haarcascade_eye.xml')
-            color = (0, 0, 0)
-            thickness = -1
-            
-            for foo in camera.capture_continuous(stream, 'jpeg', use_video_port=True):
-                # store frame
-                stream.seek(0)
-                # cls.frame = stream.read()
-                image = stream.read()
-                gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-                eye = eye_cascade.detectMultiScale(gray, 1.3, 5)
-                for (x,y,w,h) in eye:
-                    image = cv2.rectangle(image, (x, y), (x + w, y + h), color, thickness)
+			stream = io.BytesIO()
+			eye_cascade = cv2.CascadeClassifier('./haarcascades/haarcascade_eye.xml')
+			color = (0, 0, 0)
+			thickness = -1
+			
+			for foo in camera.capture_continuous(stream, 'jpeg', use_video_port=True):
+				# store frame
+				stream.seek(0)
+				# cls.frame = stream.read()
+				image = stream.read()
+				gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+				eye = eye_cascade.detectMultiScale(gray, 1.3, 5)
+				for (x,y,w,h) in eye:
+					image = cv2.rectangle(image, (x, y), (x + w, y + h), color, thickness)
 
-                cls.frame = image
+				cls.frame = image
 
-                # reset stream for next frame
-                stream.seek(0)
-                stream.truncate()
+				# reset stream for next frame
+				stream.seek(0)
+				stream.truncate()
 
-                # if there hasn't been any clients asking for frames in
-                # the last 10 seconds stop the thread
-                if time.time() - cls.last_access > 10:
-                    break
-        cls.thread = None
+				# if there hasn't been any clients asking for frames in
+				# the last 10 seconds stop the thread
+				if time.time() - cls.last_access > 10:
+					break
+		cls.thread = None

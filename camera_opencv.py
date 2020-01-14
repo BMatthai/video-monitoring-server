396e891a7055824a57a350e6c0554a3f4ef46363
Bastien MATTHAI <bastien_matthai1@hotmail.fr>
2020-01-14 00:14:27 +0000

    wip 2

--- camera_opencv.py
+++ camera_opencv.py
@@ -1,6 +1,7 @@
 import os
 import cv2
 from base_camera import BaseCamera
+import imutils
 
 
 class Camera(BaseCamera):
@@ -27,35 +28,35 @@ class Camera(BaseCamera):
 		thickness = 3
 		fullbody_cascade = cv2.CascadeClassifier('/home/pi/video-monitoring-server/haarcascades/haarcascade_fullbody.xml')
 		cars_cascade = cv2.CascadeClassifier('/home/pi/video-monitoring-server/haarcascades/haarcascade_cars.xml')
-		last_img = None
+		_, last_img = camera.read()
+		_, img = camera.read()
 		while True:
 			# read current frame
 			_, img = camera.read()
 			img = cv2.flip(img, -1)
-			
-			if (last_img != None and img != None):
-				img = imutils.resize(img, width=500)
-				gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-				gray = cv2.GaussianBlur(gray, (21, 21), 0)
 
-				last_img = imutils.resize(last_img, width=500)
-				last_gray = cv2.cvtColor(last_img, cv2.COLOR_BGR2GRAY)
-				last_gray = cv2.GaussianBlur(last_gray, (21, 21), 0)
+			img = imutils.resize(img, width=500)
+			gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+			gray = cv2.GaussianBlur(gray, (21, 21), 0)
 
-				frameDelta = cv2.absdiff(last_gray, gray)
-				thresh = cv2.threshold(frameDelta, 25, 255, cv2.THRESH_BINARY)[1]
+			last_img = imutils.resize(last_img, width=500)
+			last_gray = cv2.cvtColor(last_img, cv2.COLOR_BGR2GRAY)
+			last_gray = cv2.GaussianBlur(last_gray, (21, 21), 0)
 
-				thresh = cv2.dilate(thresh, None, iterations=2)
-				cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,
-				cv2.CHAIN_APPROX_SIMPLE)
-				cnts = imutils.grab_contours(cnts)
+			frameDelta = cv2.absdiff(last_gray, gray)
+			thresh = cv2.threshold(frameDelta, 25, 255, cv2.THRESH_BINARY)[1]
 
-				for c in cnts:
-					if cv2.contourArea(c) < args["min_area"]:
-						continue
-					(x, y, w, h) = cv2.boundingRect(c)
-					cv2.rectangle(frameDelta, (x, y), (x + w, y + h), (0, 255, 0), 2)
-					text = "Occupied"
+			thresh = cv2.dilate(thresh, None, iterations=2)
+			cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,
+			cv2.CHAIN_APPROX_SIMPLE)
+			cnts = imutils.grab_contours(cnts)
+
+			for c in cnts:
+				if cv2.contourArea(c) < 2:
+					continue
+				(x, y, w, h) = cv2.boundingRect(c)
+				cv2.rectangle(thresh, (x, y), (x + w, y + h), (255, 255, 128), 2)
+
 			# fullbody = fullbody_cascade.detectMultiScale(img, 1.3, 5)
 			# cars = cars_cascade.detectMultiScale(img, 1.3, 5)
 			# for (x,y,w,h) in fullbody:

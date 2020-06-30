6d49fd01224e77e76b3ddd5cb09298a17c998f15
Bastien <bastien_matthai1@hotmail.fr>
2020-06-30 22:32:55 +0200

    test time sleep

--- src/pystream.py
+++ src/pystream.py
@@ -13,6 +13,7 @@ app = Flask(__name__)
 def gen(camera):
 	while True:
 		frame = camera.get_frame()
+		time.sleep(0.1)
 		yield (b'--frame\r\n'b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
 @app.route('/video_feed')
